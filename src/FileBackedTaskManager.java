c1005bddf9bcfa7e44b4e1f78b007a1c8de49a76
sofia <1132236043@pfur.ru>
2025-09-20 00:16:18 +0300

    Добавить реализацию методов fromString и loadFromFile. Добавить тесты

--- src/FileBackedTaskManager.java
+++ src/FileBackedTaskManager.java
@@ -1,25 +1,25 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
-public class FileBackedTaskManager extends InMemoryTaskManager{
-    FileWriter fileHistory;
+public class FileBackedTaskManager extends InMemoryTaskManager {
+    private final String filePath;
 
-    public FileBackedTaskManager(FileWriter fileHistory) {
-        this.fileHistory = fileHistory;
+    public FileBackedTaskManager(String path) {
+        this.filePath = path;
     }
 
-    public void save(){
-        try (BufferedWriter bufferHistory = new BufferedWriter(fileHistory)){
-            bufferHistory.write("id,type,name,status,description,epic");
+    public void save() {
+        try (BufferedWriter bufferHistory = new BufferedWriter(new FileWriter(filePath))) {
+            bufferHistory.write("id,type,name,status,description,epic\n");
 
-            for (Task task : super.getTasks()){
+            for (Task task : super.getTasks()) {
                 bufferHistory.write(toString(task));
             }
-            for (Epic epic : super.getEpics()){
+            for (Epic epic : super.getEpics()) {
                 bufferHistory.write(toString(epic));
             }
-            for (Subtask subtask : super.getSubtasks()){
+            for (Subtask subtask : super.getSubtasks()) {
                 bufferHistory.write(toString(subtask));
             }
 
@@ -35,72 +35,71 @@ public class FileBackedTaskManager extends InMemoryTaskManager{
     }
 
     @Override
-    public void deleteTasks(){
+    public void deleteTasks() {
         super.deleteTasks();
         save();
     }
 
     @Override
-    public void deleteTaskById(int id){
+    public void deleteTaskById(int id) {
         super.deleteTaskById(id);
         save();
     }
 
     @Override
-    public void updateTask(Task task){
+    public void updateTask(Task task) {
         super.updateTask(task);
         save();
     }
 
     @Override
-    public void addSubtask(Subtask subtask){
+    public void addSubtask(Subtask subtask) {
         super.addSubtask(subtask);
         save();
     }
 
     @Override
-    public void deleteSubtaskById(int id){
+    public void deleteSubtaskById(int id) {
         super.deleteSubtaskById(id);
         save();
     }
 
     @Override
-    public void deleteSubtasks(){
+    public void deleteSubtasks() {
         super.deleteSubtasks();
         save();
     }
 
     @Override
-    public void updateSubtask(Subtask subtask){
+    public void updateSubtask(Subtask subtask) {
         super.updateSubtask(subtask);
         save();
     }
 
     @Override
-    public void addEpic(Epic epic){
+    public void addEpic(Epic epic) {
         super.addEpic(epic);
         save();
     }
 
     @Override
-    public void deleteEpicById(int id){
+    public void deleteEpicById(int id) {
         super.deleteEpicById(id);
         save();
     }
 
     @Override
-    public void deleteAllEpics(){
+    public void deleteAllEpics() {
         super.deleteAllEpics();
         save();
     }
 
     @Override
-    public void updateEpic(Epic epic){
+    public void updateEpic(Epic epic) {
         super.updateEpic(epic);
         save();
     }
 
-    @Override
     public String toString(Task task) {
         String line = task.getId() + ","
                 + task.getType() + ","
@@ -113,6 +112,59 @@ public class FileBackedTaskManager extends InMemoryTaskManager{
             return line + "," + subtask.getEpicId() + "\n";
         } else {
             return line + "\n";
+        }
+    }
+
+    public Task fromString(String value) {
+        String[] split = value.split(",");
+
+        int id = Integer.parseInt(split[0]);
+        Type type = Type.valueOf(split[1]);
+        String name = split[2];
+        Status status = Status.valueOf(split[3]);
+        String description = split[4];
+
+        switch (type) {
+            case Subtask:
+                return new Subtask(name, description, status, id, (int) Integer.parseInt(split[5]));
+            case Task:
+                return new Task(name, description, status, id);
+            case Epic:
+                return new Epic(name, description, status, id, new ArrayList<>());
+            default:
+                throw new IllegalArgumentException("Не верный тип задачи");
+        }
+    }
+
+    static FileBackedTaskManager loadFromFile(File file) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager(file.getPath());
+            {
+                String header = br.readLine();
+                while (br.ready()) {
+                    String line = br.readLine();
+                    Task task = fileBackedTaskManager.fromString(line);
+                    switch (task.getType()) {
+                        case Subtask:
+                            fileBackedTaskManager.addSubtask((Subtask) task);
+                            break;
+                        case Task:
+                            fileBackedTaskManager.addTask(task);
+                            break;
+                        case Epic:
+                            fileBackedTaskManager.addEpic((Epic) task);
+                            break;
+                        default:
+                            System.out.println("В файле присутствует задача несуществующего типа");
+                    }
+                }
+                return fileBackedTaskManager;
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
---
+++ test/FileBackedTaskManagerTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FileBackedTaskManagerTest {
+
+    @Test
+    public void testFileOperations (){
+        try {
+            File file = File.createTempFile("task_manager_test", ".csv");
+            FileBackedTaskManager fileBackedTaskManager = new FileBackedTaskManager(file.getPath());
+
+            Epic epic1 = new Epic("Эпик 1", "Описание", new ArrayList<>());
+            Subtask subtask1 = new Subtask("Подзадача 1", "Описание", epic1.getId());
+            Task task1 = new Task("Задача 1", "Описание задачи 1");
+
+            fileBackedTaskManager.addEpic(epic1);
+            fileBackedTaskManager.addTask(task1);
+            fileBackedTaskManager.addSubtask(subtask1);
+
+            String fileRead = Files.readString(file.toPath());
+
+            assertTrue(file.exists());
+            assertTrue(fileRead.contains("Эпик 1"), "Должен быть эпик");
+            assertTrue(fileRead.contains("Задача 1"), "Должна быть задача");
+            assertTrue(fileRead.contains("Подзадача 1"), "Должна быть подзадача");
+
+        } catch (IOException e) {
+            throw new ManagerSaveException("Ошибка инициализации");
+        }
+    }
+
+    @Test
+    public void shouldSaveAndLoadEmptyFile() {
+        try {
+            File file = File.createTempFile("empty_test", ".csv");
+
+            FileBackedTaskManager manager1 = new FileBackedTaskManager(file.getPath());
+            FileBackedTaskManager manager2 = FileBackedTaskManager.loadFromFile(file);
+
+            assertTrue(manager2.getTasks().isEmpty(), "Задач не должно быть");
+            assertTrue(manager2.getEpics().isEmpty(), "Эпиков не должно быть");
+            assertTrue(manager2.getSubtasks().isEmpty(), "Подзадач не должно быть");
+
+        } catch (IOException e) {
+            throw new ManagerSaveException("Ошибка теста пустого файла");
+        }
+    }
+
+}
